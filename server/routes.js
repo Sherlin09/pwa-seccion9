f06701c6b98a75de7ce9186eeb37a026a4e7d500
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-10-27 11:42:39 -0500

    102 API REST - Get Mensajes

--- server/routes.js
+++ server/routes.js
@@ -3,14 +3,19 @@ var express = require('express');
 var router = express.Router();
 
 
-
+const mensajes = [{
+    _id: '123',
+    user: 'spiderman',
+    mensaje: 'Hola mundo'
+}];
 
 
 
 
 // Get mensajes
-router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+router.get('/', function(req, res) {
+    // res.json('Obteniendo mensajes');
+    res.json(mensajes);
 });
 
 
